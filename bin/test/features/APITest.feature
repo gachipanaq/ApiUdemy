b35c906f51ea364b34eb53b85449eb2dab9d603a
gchipana <gchipana@everis.com>
2021-11-14 12:21:47 -0500

    app

--- bin/test/features/APITest.feature
+++ bin/test/features/APITest.feature
@@ -3,4 +3,4 @@ Feature: Ejemplo de Request para Udemy
 @API
 Scenario: Prueba GET al endpoint
 Given I send a GET request to the endpoint
-Then I get a list of 10 users
+Then I get a 200 status code
--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -7,9 +7,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "Steps"
+        glue = "Steps",
+        plugin = {"com.aventsack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber-reports.json"},
+        tags = "@API"
 )
 
 public class Runner {
 
+        public static void main(String[] args) {
+                
+        }
 }
